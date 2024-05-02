cfc3f59b1ed350372e00d60699c889c4e7fc921d
Kim An <anvu734@gmail.com>
2024-05-02 17:25:32 +0700

    backend update

---
+++ backend/controllers/tourController.js
@@ -0,0 +1,19 @@
+import Tour from "../models/Tour.js";
+
+// create new tour
+export const createTour = async (req, res) => {
+  const newTour = new Tour(req.body);
+  try {
+    const savedTour = await newTour.save();
+    res.status(200).json({
+      success: true,
+      message: "Seccessfully created",
+      data: savedTour,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: "Failed to create. Try again",
+    });
+  }
+};
---
+++ backend/index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+import tourRoute from "./routers/tours.js";
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 8000;
+
+//database connection
+mongoose.set("strictQuery", false);
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("-------MongoDB database connected--------");
+  } catch (err) {
+    console.log(err);
+    console.log("Connect failed!");
+  }
+};
+
+//middware
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+app.use("/tours", tourRoute);
+
+app.listen(port, () => {
+  connect();
+  console.log(`Server is running on port ${port}`);
+});
