1ddac38a0806908cd06f9c5b6d7bb23611a9b43a
Kim An <anvu734@gmail.com>
2024-05-15 17:04:49 +0700

    fix server

--- backend/routers/users.js
+++ backend/routers/users.js
@@ -9,12 +9,11 @@ const router = express.Router();
 import { verifyAdmin, verifyUser } from "../utils/verifyToken.js";
 
 //update new user
-router.put("/:id", updateUser);
+router.put("/:id", verifyUser, updateUser);
 //delete new user
-router.delete("/:id", deleteUser);
+router.delete("/:id", verifyUser, deleteUser);
 //get single user
-router.get("/:id", getSingleUser);
+router.get("/:id", verifyUser, getSingleUser);
 //get all user
-// router.get("/", verifyAdmin, getAllUser);
-router.get("/", getAllUser);
+router.get("/", verifyAdmin, getAllUser);
 export default router;
