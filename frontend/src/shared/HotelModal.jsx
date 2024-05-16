756c2545ab3b47ff0f1abbac17a8f826bca54ca5
minhdang0 <dangnhatminh8402@gmail.com>
2024-05-17 02:47:27 +0700

    Minh push

--- frontend/src/shared/HotelModal.jsx
+++ frontend/src/shared/HotelModal.jsx
@@ -3,13 +3,46 @@ import "./HotelModal.css"
 import { Modal, ModalHeader, ModalBody } from "reactstrap";
 
 const HotelModal = ({ isOpen, toggle, hotelInfo }) => {
+
+  const renderStars = (rating) => {
+    const stars = [];
+    const fullStars = Math.floor(rating);
+    const hasHalfStar = rating - fullStars >= 0.5;
+
+    for (let i = 1; i <= 5; i++) {
+      if (i <= fullStars) {
+        stars.push(<span key={i} className="star filled">&#9733;</span>);
+      } else if (i === fullStars + 1 && hasHalfStar) {
+        stars.push(<span key={i} className="star half">&#9733;</span>);
+      } else {
+        stars.push(<span key={i} className="star">&#9733;</span>);
+      }
+    }
+    return stars;
+  };
+
   return (
     <Modal isOpen={isOpen} toggle={toggle}>
-      <ModalHeader toggle={toggle}>{hotelInfo?.name}</ModalHeader>
+      <div className="modal-title">
+        <ModalHeader toggle={toggle}> <p>Khách sạn {hotelInfo?.name}</p></ModalHeader>
+      </div>
+     
       <ModalBody>
-        <img src={`${hotelInfo?.image}`} alt={hotelInfo?.name} />
-        <p><strong>Địa chỉ:</strong> {hotelInfo?.address}</p>
-        <p><strong>Đánh giá:</strong> {hotelInfo?.rating}</p>
+
+        <div>
+          <img  className="img-hotel  max-w-lg rounded-lg" src={`${hotelInfo?.image}`} alt={hotelInfo?.name} />
+          <div className="star-rating">
+            <h3>Thông tin khách sạn</h3>
+          </div>
+          
+          <div className="body-info">
+            <div>
+            <strong>Đánh giá: </strong>{renderStars(hotelInfo?.rating)}
+            </div>
+            <p><strong>Địa chỉ:</strong> {hotelInfo?.address}</p>
+          </div>
+        </div>
+       
       </ModalBody>
     </Modal>
   );
